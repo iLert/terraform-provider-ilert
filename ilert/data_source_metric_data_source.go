7205b647388f9df505287ca3a8030fd8c1cef098
Marko Simon <marko@ilert.com>
2023-01-18 16:08:04 +0100

    add data source metric data source

---
+++ ilert/data_source_metric_data_source.go
@@ -0,0 +1,70 @@
+package ilert
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/iLert/ilert-go/v2"
+)
+
+func dataSourceMetricDataSource() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceMetricDataSourceRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func dataSourceMetricDataSourceRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	client := meta.(*ilert.Client)
+
+	log.Printf("[DEBUG] Reading ilert metric data source")
+
+	searchName := d.Get("name").(string)
+
+	err := resource.RetryContext(ctx, d.Timeout(schema.TimeoutRead), func() *resource.RetryError {
+		resp, err := client.SearchMetricDataSource(&ilert.SearchMetricDataSourceInput{MetricDataSourceName: &searchName})
+		if err != nil {
+			if _, ok := err.(*ilert.RetryableAPIError); ok {
+				time.Sleep(2 * time.Second)
+				return resource.RetryableError(fmt.Errorf("waiting for metric data source with name '%s' to be read", searchName))
+			}
+			return resource.NonRetryableError(fmt.Errorf("could not read a metric data source with name: %s", searchName))
+		}
+
+		found := resp.MetricDataSource
+
+		if found == nil {
+			return resource.NonRetryableError(
+				fmt.Errorf("unable to locate any metric data source with the name: %s", searchName),
+			)
+		}
+
+		d.SetId(strconv.FormatInt(found.ID, 10))
+		d.Set("name", found.Name)
+		d.Set("type", found.Type)
+
+		return nil
+	})
+
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
