79bddeb4d95ec3c3793d3d2d69c6a8db2ab4e928
Marko Simon <marko@ilert.com>
2023-01-18 19:20:26 +0100

    edit metric example

--- examples/metric/main.tf
+++ examples/metric/main.tf
@@ -1,5 +1,22 @@
-data "ilert_metric_data_source" "example" {
+resource "ilert_team" "example" {
   name = "example"
+}
+
+resource "ilert_metric_data_source" "example_prometheus" {
+  name = "example"
+  type = "PROMETHEUS"
+
+  team {
+    id   = ilert_team.example.id
+    name = "example"
+  }
+
+  metadata {
+    auth_type  = "BASIC"
+    basic_user = "your prometheus user name"
+    basic_pass = "your prometheus password"
+    url        = "your prometheus url"
+  }
 }
 
 resource "ilert_metric" "example" {
@@ -10,6 +27,6 @@ resource "ilert_metric" "example" {
     query = "your query"
   }
   data_source {
-    id = data.ilert_metric_data_source.example.id
+    id = ilert_metric_data_source.example.id
   }
 }
