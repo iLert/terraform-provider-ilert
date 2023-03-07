a679728e78a4cb4fb10f2692c99ea963cc0a3d42
Marko Simon <marko@ilert.com>
2023-03-07 03:04:32 +0100

    add example for user duty preference

---
+++ examples/user_duty_preference/main.tf
@@ -0,0 +1,24 @@
+resource "ilert_user" "example" {
+  email      = "example@example.com"
+  first_name = "example"
+  last_name  = "example"
+}
+
+resource "ilert_user_email_contact" "example" {
+  target = "example@example.com"
+  user {
+    id = ilert_user.example.id
+  }
+}
+
+resource "ilert_user_duty_preference" "example" {
+  method = "EMAIL"
+  contact {
+    id = ilert_user_email_contact.example.id
+  }
+  before_min = 0
+  type       = "ON_CALL"
+  user {
+    id = ilert_user.example.id
+  }
+}
