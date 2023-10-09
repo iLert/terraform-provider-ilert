6e5975223d452486f6ebb4d04e3f17a0f1712d83
Marko Simon <marko@ilert.com>
2023-10-09 15:17:05 +0200

    fix broken e2e incident template

--- examples/e2e/module/incident_template.tf
+++ examples/e2e/module/incident_template.tf
@@ -2,4 +2,5 @@ resource "ilert_incident_template" "this" {
   name              = var.name
   status            = "INVESTIGATING"
   send_notification = true
+  summary           = "example summary"
 }
